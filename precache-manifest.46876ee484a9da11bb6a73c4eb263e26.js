26072cb9a57e817c2d382bd810b22134710ff24a
PrateekVerma20 <prateekverma1998@gmail.com>
2020-04-27 15:52:03 +0530

    Updates

---
+++ precache-manifest.46876ee484a9da11bb6a73c4eb263e26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e4a8736d23bc4634bd8c5964c08881",
+    "url": "/MyPortfolioSite/index.html"
+  },
+  {
+    "revision": "f17a85383bf11b376af1",
+    "url": "/MyPortfolioSite/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "e8d801d2e560b575381e",
+    "url": "/MyPortfolioSite/static/css/main.4841bd5f.chunk.css"
+  },
+  {
+    "revision": "f17a85383bf11b376af1",
+    "url": "/MyPortfolioSite/static/js/2.0c4b7515.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/MyPortfolioSite/static/js/2.0c4b7515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d801d2e560b575381e",
+    "url": "/MyPortfolioSite/static/js/main.e78740df.chunk.js"
+  },
+  {
+    "revision": "19fd85a579afe40d7e8e",
+    "url": "/MyPortfolioSite/static/js/runtime-main.346caf37.js"
+  }
+]);
